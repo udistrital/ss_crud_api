ec38d895b99a083fc1ef2ce55ab328a2cb498624
jcamilosarmientor <jcamilosarmientor@gmail.com>
2017-12-04 13:50:23 -0500

    Corregida transacción para reigstro de pagos

--- models/tr_pago.go
+++ models/tr_pago.go
@@ -7,23 +7,20 @@ import (
 )
 
 type TrPeriodoPago struct {
-	PeriodoPago PeriodoPago
-	Pago        []*Pago
+	PeriodoPago *PeriodoPago
+	Pagos       []*Pago
 }
 
 // Transancción para el registro de pagos
 func RegistrarPagos(m *TrPeriodoPago) (alerta []string, err error) {
 	o := orm.NewOrm()
 	o.Begin()
-	id, err := o.Insert(m.PeriodoPago)
 
-	if err == nil {
-		err = o.Rollback()
-		fmt.Println("Pagos: ", m.Pago)
-		for _, v := range m.Pago {
+	if id, err := o.Insert(m.PeriodoPago); err == nil {
+		for _, v := range m.Pagos {
 			v.PeriodoPago = &PeriodoPago{Id: int(id)}
-
 			if _, err = o.Insert(v); err != nil {
+				fmt.Println("rollback")
 				o.Rollback()
 				alerta[0] = "error"
 				alerta = append(alerta, "Error registrado cada pago")
