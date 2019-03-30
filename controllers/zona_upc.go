da6aea4c14a4921c4cfa9245cccbc33c5d78860d
corio27 <corio27@gmail.com>
2019-03-30 05:49:26 +0000

    ajuste error beego

--- controllers/zona_upc.go
+++ controllers/zona_upc.go
@@ -5,7 +5,7 @@ import (
 	"errors"
 	"strconv"
 	"strings"
-
+	"fmt"
 	"github.com/udistrital/ss_crud_api/models"
 
 	"github.com/astaxie/beego"
@@ -39,11 +39,13 @@ func (c *ZonaUpcController) Post() {
 			c.Ctx.Output.SetStatus(201)
 			c.Data["json"] = v
 		} else {
-			beego.Error(err)
+			fmt.Println(err)
+			//beego.Error(err)
 			c.Abort("400")
 		}
 	} else {
-		beego.Error(err)
+		fmt.Println(err)
+		//beego.Error(err)
 		c.Abort("400")
 	}
 	c.ServeJSON()
@@ -61,7 +63,8 @@ func (c *ZonaUpcController) GetOne() {
 	id, _ := strconv.Atoi(idStr)
 	v, err := models.GetZonaUpcById(id)
 	if err != nil {
-		beego.Error(err)
+		fmt.Println(err)
+		//beego.Error(err)
 		c.Abort("404")
 	} else {
 		c.Data["json"] = v
@@ -125,7 +128,8 @@ func (c *ZonaUpcController) GetAll() {
 
 	l, err := models.GetAllZonaUpc(query, fields, sortby, order, offset, limit)
 	if err != nil {
-		beego.Error(err)
+		fmt.Println(err)
+		//beego.Error(err)
 		c.Abort("404")
 	} else {
 		if l == nil {
@@ -152,11 +156,13 @@ func (c *ZonaUpcController) Put() {
 		if err := models.UpdateZonaUpcById(&v); err == nil {
 			c.Data["json"] = v
 		} else {
-			beego.Error(err)
+			fmt.Println(err)
+			//beego.Error(err)
 			c.Abort("400")
 		}
 	} else {
-		beego.Error(err)
+		fmt.Println(err)
+		//beego.Error(err)
 		c.Abort("400")
 	}
 	c.ServeJSON()
@@ -175,7 +181,8 @@ func (c *ZonaUpcController) Delete() {
 	if err := models.DeleteZonaUpc(id); err == nil {
 		c.Data["json"] = map[string]interface{}{"Id": id}
 	} else {
-		beego.Error(err)
+		fmt.Println(err)
+		//beego.Error(err)
 		c.Abort("404")
 	}
 	c.ServeJSON()
